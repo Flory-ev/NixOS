482f8dc1de46b670cd7be0026a3462d2c3facc50
F <vladislavtkachuk@yahoo.com>
2026-01-03 21:29:21 +0100

    -

--- modules/system.nix
+++ modules/system.nix
@@ -16,8 +16,6 @@
     alsa.enable = true;
   };
 
-  programs.hyprland.enable = true;
-
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
