1300c7a8939e3b732e342162428767370dd9381f
F <vladislavtkachuk@yahoo.com>
2026-01-18 00:16:48 +0100

    -

--- modules/software.nix
+++ modules/software.nix
@@ -1,75 +1,34 @@
 { config, lib, pkgs, ... }:
+
 {
-	
-	#Packages
   environment.systemPackages = with pkgs; [
-  #A
-
-  #B
-		bat btop
-	#C
+    bat btop
     chromium curl
-  #D
-		discord
-  #E
-		eza
-  #F
-		fastfetch fd firefox fzf
-  #G
-		gamemode gcc git
-  #H
-
-  #I
-
-  #J
-
-	#K
+    discord
+    eza
+    fastfetch fd firefox fzf
+    gamemode gcc
     kdePackages.partitionmanager kitty
-	#L
     lazygit lutris
-  #M
-
-	#N
     nodejs
-  #O
-
-	#P
-
-	#Q
     qbittorrent
-	#R
-    ripgrep reaper
-	#S
+    reaper ripgrep
     spotify
-	#T
-    telegram-desktop tor-browser tldr tree
-  #U
-		unzip
-	#V
+    telegram-desktop tldr tor-browser tree
+    unzip
     vim vscodium
-	#W
     wget
-  #X
-
-  #Y
-
-	#Z
     zoxide
   ];
 
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	#Programs
-  programs.appimage = {
-    enable = true;
-    binfmt = true;
+  programs = {
+    appimage = { enable = true; binfmt = true; };
+    gamemode.enable = true;
+    steam.enable = true;
+    zsh.enable = true;
   };
 
-  programs.steam.enable = true;
-  programs.gamemode.enable = true;
-  programs.zsh.enable = true;
-
-	#Services
   services.flatpak.enable = true;
-
 }
