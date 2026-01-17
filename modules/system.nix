54572b9fa98630721a690373b9c5b5d5d1a56eb8
F <vladislavtkachuk@yahoo.com>
2026-01-18 00:35:00 +0100

    -

--- modules/system.nix
+++ modules/system.nix
@@ -1,53 +1,52 @@
 { config, lib, pkgs, ... }:
-{
-  
-	boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+{
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/Copenhagen";
 
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-  };
-	
-	services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.displayManager.sddm.settings.General.DisplayServer = "wayland";
-  #services.displayManager.cosmic-greeter.enable = true;
-  
-	services.desktopManager.plasma6.enable = true;
-  
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    elisa
-    gwenview
-    kwalletmanager
-    okular
-  ];
-
-	services.desktopManager.cosmic.enable = true;
-
-	environment.cosmic.excludePackages = with pkgs; [
-    
-  ];
-	
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/f/nixos";
-  };
-	
   nix = {
     settings.experimental-features = [ "flakes" "nix-command" ];
     settings.auto-optimise-store = true;
   };
 
+  programs.nh = {
+    enable = true;
+    clean = { enable = true; extraArgs = "--keep-since 4d --keep 3"; };
+    flake = "/home/f/nixos";
+  };
+
+  services = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      settings.General.DisplayServer = "wayland";
+    };
+    desktopManager = {
+      plasma6.enable = true;
+      cosmic.enable = true;
+    };
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+    };
+  };
+
+  environment = {
+    plasma6.excludePackages = with pkgs.kdePackages; [ elisa gwenview kwalletmanager okular ];
+    cosmic.excludePackages = [ ];
+  };
+
+  security.rtkit.enable = true;
   system.stateVersion = "25.05";
 }
