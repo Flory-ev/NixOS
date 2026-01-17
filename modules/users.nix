e0a9e642eebf64e3ee69e073cca0a544d3622712
F <vladislavtkachuk@yahoo.com>
2026-01-18 00:26:19 +0100

    -

--- modules/users.nix
+++ modules/users.nix
@@ -1,12 +1,11 @@
 { config, lib, pkgs, ... }:
+
 {
-  
-	users.users.f = {
+  users.users.f = {
     isNormalUser = true;
     home = "/home/f";
     shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" "libvirtd" "kvm" ];
+    extraGroups = [ "kvm" "libvirtd" "networkmanager" "wheel" ];
     initialHashedPassword = "$y$j9T$8T41ml.08LAvNa02/0eUV.$uLjb1z6VegRqepV9Dr02j8mopvUJJag/pd9I9oBr258";
   };
-
 }
