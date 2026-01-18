d5c2655bfc3efc5226ce9f8dff50d5e3e4bc3edc
F <vladislavtkachuk@yahoo.com>
2026-01-18 20:04:37 +0100

    -

--- system/locale.nix
+++ system/locale.nix
@@ -1,8 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
 
-time.timeZone = "Europe/Copenhagen";
-i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "Europe/Copenhagen";
 
+  services.xserver.xkb = {
+    layout = "us,ru";
+    options = "grp:alt_shift_toggle";
+  
 }
