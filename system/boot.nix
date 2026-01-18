9f1b9696c959c815efbfef17def76151f2a71f2c
F <vladislavtkachuk@yahoo.com>
2026-01-18 19:53:10 +0100

    -

--- system/boot.nix
+++ system/boot.nix
@@ -1,10 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.kernelParams = [ "quiet" "splash" ];
 
-boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
+  boot.loader = {
+    efi = { canTouchEfiVariables = true; efiSysMountPoint = "/boot";
+    };
+
+    systemd-boot = { configurationLimit = 10; editor = false; enable = true;
+    };
+    timeout = 5;
+
+  boot.plymouth.enable = true;
 
 }
