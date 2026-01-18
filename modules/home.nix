831e646818f1086e939a76554fff1a1df21ed3e3
F <vladislavtkachuk@yahoo.com>
2026-01-18 12:17:37 +0100

    -

--- modules/home.nix
+++ modules/home.nix
@@ -10,7 +10,8 @@
     direnv = { enable = true; nix-direnv.enable = true; };
     fzf = { enable = true; enableZshIntegration = true; };
     git = { enable = true; settings.init.defaultBranch = "Main"; settings.user = { name = "F"; email = "vladislavtkachuk@yahoo.com"; }; };
-    zoxide = { enable = true; enableZshIntegration = true; options = [ "--cmd cd" ]; };
+    nh = { enable = true; clean = enable = true; extraArgs = "--keep-since 4d --keep 3"; flake = "/home/f/nixos"; };
+		zoxide = { enable = true; enableZshIntegration = true; options = [ "--cmd cd" ]; };
     zsh = { enable = true; oh-my-zsh = { enable = true; plugins = [ "git" ]; theme = "robbyrussell"; }; shellAliases = { cat = "bat"; ll = "eza -lah --icons"; ls = "eza"; ns = "nix-shell"; };
     };
   };
