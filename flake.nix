02add64b04b4fc2df6169d36c536082ae4a591f4
F <vladislavtkachuk@yahoo.com>
2026-01-18 15:06:13 +0100

    -

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system = "x86_64-linux";
       modules = [
         ./hardware.nix
-        ./system/software.nix
+        ./user/software.nix
         ./system/system.nix
         ./modules/users.nix
         ./system/virtualization.nix
