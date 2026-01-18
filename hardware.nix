7ce0a1addce738fc0f7ba50a8a2a77bc42d3820c
F <vladislavtkachuk@yahoo.com>
2026-01-18 01:34:17 +0100

    -

--- hardware.nix
+++ hardware.nix
@@ -1,32 +1,29 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "uas" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "uas" "sd_mod" "rtsx_pci_sdmmc" ];
+    kernelModules = [ "kvm-intel" ];
+  };
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ca75ba4b-516a-405f-809e-0c82714f19b0";
-      fsType = "ext4";
+  fileSystems = {
+    "/" = { 
+      device = "/dev/disk/by-uuid/ca75ba4b-516a-405f-809e-0c82714f19b0"; 
+      fsType = "ext4"; 
     };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5C23-722E";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+    "/boot" = { 
+      device = "/dev/disk/by-uuid/5C23-722E"; 
+      fsType = "vfat"; 
+      options = [ "fmask=0077" "dmask=0077" ]; 
     };
+  };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/e549d523-3a24-4e2f-85dc-b38c1bfb46f4"; }
-    ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/e549d523-3a24-4e2f-85dc-b38c1bfb46f4"; } ];
 
-  networking.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  
+  networking.useDHCP = lib.mkDefault true;
 }
