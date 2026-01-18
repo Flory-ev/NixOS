9fbaf9fc9f13ae916f7b7e84c87f68cc671b17d3
F <vladislavtkachuk@yahoo.com>
2026-01-18 08:58:20 +0100

    -

--- modules/home.nix
+++ modules/home.nix
@@ -4,24 +4,15 @@
   home = {
     username = "f";
     homeDirectory = "/home/f";
-    stateVersion = "25.05";
   };
 
   programs = {
     direnv = { enable = true; nix-direnv.enable = true; };
     fzf = { enable = true; enableZshIntegration = true; };
-    zoxide = { enable = true; enableZshIntegration = true; options = [ "--cmd cd" ]; };
-
-    git = {
-      enable = true;
-      settings.init.defaultBranch = "Main";
-      settings.user = { name = "F"; email = "vladislavtkachuk@yahoo.com"; };
-    };
-
-    zsh = {
-      enable = true;
-      oh-my-zsh = { enable = true; plugins = [ "git" ]; theme = "robbyrussell"; };
-      shellAliases = { cat = "bat"; ll = "eza -lah --icons"; ls = "eza"; ns = "nix-shell"; };
+    git = { enable = true; settings.init.defaultBranch = "Main"; settings.user = { name = "F"; email = "vladislavtkachuk@yahoo.com"; };
+		zoxide = { enable = true; enableZshIntegration = true; options = [ "--cmd cd" ]; };
+    zsh = { enable = true; oh-my-zsh = { enable = true; plugins = [ "git" ]; theme = "robbyrussell"; }; shellAliases = { cat = "bat"; ll = "eza -lah --icons"; ls = "eza"; ns = "nix-shell"; };
     };
   };
+	stateVersion = "25.05";
 }
