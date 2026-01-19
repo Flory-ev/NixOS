ddc7fe5266e7dcbbda546a8e86b5b041f2d53c80
F <vladislavtkachuk@yahoo.com>
2026-01-19 18:58:44 +0100

    -

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -9,34 +9,26 @@
   environment.systemPackages = with pkgs; [
     OVMF
     qemu
-    spice spice-gtk spice-protocol
-    virt-manager virt-viewer virtio-win
-		win-spice
+    spice
+    spice-gtk
+    spice-protocol
+    virt-manager
+    virt-viewer
+    virtio-win
+    win-spice
   ];
 
-  networking = {
-    bridges.br0.interfaces = [ ];
-    interfaces.br0.useDHCP = true;
-  };
-
   programs.virt-manager.enable = true;
-
-  systemd.services.libvirtd-default-network = {
-    after = [ "libvirtd.service" ];
-    description = "Setup libvirt default network";
-    requires = [ "libvirtd.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = { RemainAfterExit = "yes"; Type = "oneshot"; };
-    script = ''
-      ${pkgs.libvirt}/bin/virsh net-autostart default || true
-      ${pkgs.libvirt}/bin/virsh net-start default || true
-    '';
-  };
 
   virtualisation = {
     libvirtd = {
       enable = true;
-      qemu = { package = pkgs.qemu_kvm; runAsRoot = true; swtpm.enable = true; };
+      qemu = {
+        ovmf.enable = true;
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+      };
     };
     spiceUSBRedirection.enable = true;
   };
