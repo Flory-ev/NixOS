f108cf6e116c56df2e2fce8a8db2c930ef6b1287
F <vladislavtkachuk@yahoo.com>
2026-01-19 18:40:24 +0100

    -

--- system/networking.nix
+++ system/networking.nix
@@ -1,10 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
-
   networking = {
+    firewall = {
+      allowedTCPPorts = [ ];
+      allowedUDPPorts = [ ];
+      enable = true;
+    };
+
     hostName = "nixos";
-    networkmanager.enable = true;
+    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+
+    networkmanager = {
+      enable = true;
+      wifi.powersave = false;
+    };
   };
 
+  services.resolved = {
+    dnssec = "true";
+    domains = [ "~." ];
+    enable = true;
+    fallbackDns = [ "1.1.1.1" "1.0.0.1" ];
+  };
 }
