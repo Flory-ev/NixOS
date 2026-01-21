2da5a2f86f42504b58362c1ec9593f4d72e8b996
F <vladislavtkachuk@yahoo.com>
2026-01-21 19:13:19 +0100

    -

--- system/networking.nix
+++ system/networking.nix
@@ -20,9 +20,9 @@
   services.resolved = {
     enable = true; 
     settings = {
+      Resolve.FallbackDNS = [ "1.1.1.1" "1.0.0.1" ];
       Resolve.dnssec = "true";
       Resolve.Domains = [ "~." ];
-      Resolve.FallbackDNS = [ "1.1.1.1" "1.0.0.1" ];
     };    
   };
 }
