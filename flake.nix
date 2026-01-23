61cdabab42a70c608ee1223310bb7f37ffe3df2a
F <vladislavtkachuk@yahoo.com>
2026-01-23 17:11:29 +0100

    -

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { nixpkgs, home-manager, ... }@inputs: 
   let
-    mkSystem = host: system: nixpkgs.lib.nixosSystem {
+    mkSystem = host: user: system: nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit inputs; }; 
       modules = [
@@ -23,17 +23,17 @@
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
-            users.f = import ./modules/user/home.nix;
+            users.${user} = import ./modules/user/home.nix;
           };
         }
       ];
     };
   in {
     nixosConfigurations = {
-      solar-flare = mkSystem "solar-flare" "x86_64-linux";
-      vortex = mkSystem "vortex" "x86_64-linux";
-      nebula = mkSystem "nebula" "x86_64-linux";
-      stardust = mkSystem "stardust" "x86_64-linux";
+      solar-flare = mkSystem "solar-flare" "f" "x86_64-linux";
+      vortex = mkSystem "vortex" "f" "x86_64-linux";
+      nebula = mkSystem "nebula" "f" "x86_64-linux";
+      stardust = mkSystem "stardust" "f" "x86_64-linux";
     };
   };
 }
