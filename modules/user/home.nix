0a8eaa8b889b549b9b1265cf19bcc0997dd7c45f
F <vladislavtkachuk@yahoo.com>
2026-01-25 12:56:09 +0100

    -

--- modules/user/home.nix
+++ modules/user/home.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  user,
-  ...
-}:
+{ user, ... }:
 
 {
   imports = [
--- modules/user/programs.nix
+++ modules/user/programs.nix
@@ -1,11 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  user,
-  ...
-}:
+{ pkgs, user, ... }:
 
 {
   programs = {
