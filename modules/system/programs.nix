4a06d3fccadc2d81ee7d10a92fda6e75b4419663
F <vladislavtkachuk@yahoo.com>
2026-01-26 16:38:16 +0100

    -

--- modules/system/programs.nix
+++ modules/system/programs.nix
@@ -1,19 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-
   programs = {
     appimage = {
       enable = true;
       binfmt = true;
     };
-    gamemode = {
-      enable = true;
-    };
+    
+    gamemode.enable = true;
+    
     steam = {
       enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
+    
     zsh.enable = true;
   };
-
 }
