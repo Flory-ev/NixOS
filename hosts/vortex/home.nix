276c26d7c5add01433e501f950dcd6984ab2dd6e
F <vladislavtkachuk@yahoo.com>
2026-01-27 21:49:07 +0100

    -

--- hosts/vortex/home.nix
+++ hosts/vortex/home.nix
@@ -2,14 +2,14 @@
 {
   home-manager.users.f = {
     imports = [
-      ../../modules/user/packages.nix
-      ../../modules/user/programs/direnv.nix
-      ../../modules/user/programs/fzf.nix
-      ../../modules/user/programs/git.nix
-      ../../modules/user/programs/nh.nix
-      ../../modules/user/programs/starship.nix
-      ../../modules/user/programs/zoxide.nix
-      ../../modules/user/programs/zsh.nix
+      ../../modules/home/packages.nix
+      ../../modules/home/programs/direnv.nix
+      ../../modules/home/programs/fzf.nix
+      ../../modules/home/programs/git.nix
+      ../../modules/home/programs/nh.nix
+      ../../modules/home/programs/starship.nix
+      ../../modules/home/programs/zoxide.nix
+      ../../modules/home/programs/zsh.nix
     ];
 
     home = {
