1ad24091f56552fbfbd7fdf69af633023aca3d2c
F <vladislavtkachuk@yahoo.com>
2026-01-27 21:44:46 +0100

    -

--- hosts/vortex/home.nix
+++ hosts/vortex/home.nix
@@ -1,30 +1,21 @@
 { inputs, ... }:
-
 {
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    extraSpecialArgs = {
-      inherit inputs;
-      user = "f";
-    };
-    users.f = {
-      imports = [
-        ../../modules/user/packages.nix
-        ../../modules/user/programs/direnv.nix
-        ../../modules/user/programs/fzf.nix
-        ../../modules/user/programs/git.nix
-        ../../modules/user/programs/nh.nix
-        ../../modules/user/programs/starship.nix
-        ../../modules/user/programs/zoxide.nix
-        ../../modules/user/programs/zsh.nix
-      ];
+  home-manager.users.f = {
+    imports = [
+      ../../modules/user/packages.nix
+      ../../modules/user/programs/direnv.nix
+      ../../modules/user/programs/fzf.nix
+      ../../modules/user/programs/git.nix
+      ../../modules/user/programs/nh.nix
+      ../../modules/user/programs/starship.nix
+      ../../modules/user/programs/zoxide.nix
+      ../../modules/user/programs/zsh.nix
+    ];
 
-      home = {
-        username = "f";
-        homeDirectory = "/home/f";
-        stateVersion = "25.05";
-      };
+    home = {
+      username = "f";
+      homeDirectory = "/home/f";
+      stateVersion = "24.05";
     };
   };
 }
