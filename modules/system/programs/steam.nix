4ee4b4c11279f9caa6f42ca645087ee954e6ce2d
F <vladislavtkachuk@yahoo.com>
2026-01-27 07:47:56 +0100

    -

--- modules/system/programs/steam.nix
+++
@@ -1,9 +0,0 @@
-		steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
-    };
