c7a2898d1b588d89d0a0bf173d4d905226928605
F <vladislavtkachuk@yahoo.com>
2026-01-28 18:46:47 +0100

    -

--- system/core/settings.nix
+++ system/core/settings.nix
@@ -14,6 +14,12 @@
       ];
     };
 
+		gc = {
+		  automatic = true;
+		  dates = "weekly";
+		  options = "--delete-older-than 7d";
+		};
+
     optimise = {
       automatic = true;
       dates = [ "weekly" ];
