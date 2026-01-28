4134f7473398e60860b7b461162621c9f6feeaf6
F <vladislavtkachuk@yahoo.com>
2026-01-28 12:43:07 +0100

    -

--- hosts/vortex/default.nix
+++ hosts/vortex/default.nix
@@ -9,8 +9,8 @@
     ./hardware-configuration.nix
     ../../profiles/laptop.nix
     inputs.home-manager.nixosModules.home-manager
-    ./host-modules/networking.nix
-    ./host-modules/users.nix
-    ./host-modules/home.nix
+    ./home.nix
+    ./networking.nix
+    ./users.nix
   ];
 }
