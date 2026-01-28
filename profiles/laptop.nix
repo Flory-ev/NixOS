2d900c3462156ac1a19079381424b163aa2d4b06
F <vladislavtkachuk@yahoo.com>
2026-01-28 18:59:35 +0100

    -

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }:
-
 {
   imports = [
     ./base.nix
@@ -12,7 +11,75 @@
     ../system/hardware/graphics.nix
   ];
 
-  fonts.packages = with pkgs; [
+  boot.kernelParams = [ "nvme_core.default_ps_max_latency_us=0" ];
+
+	programs.light.enable = true;
+
+  services = {
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+
+        START_CHARGE_THRESH_BAT0 = 20;
+        STOP_CHARGE_THRESH_BAT0 = 80;
+
+        USB_AUTOSUSPEND = 1;
+        USB_EXCLUDE_AUDIO = 1;
+
+        DISK_DEVICES = "nvme0n1 sda";
+        DISK_APM_LEVEL_ON_AC = "254 254";
+        DISK_APM_LEVEL_ON_BAT = "128 128";
+
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
+
+        RUNTIME_PM_ON_AC = "on";
+        RUNTIME_PM_ON_BAT = "auto";
+      };
+    };
+
+    thermald.enable = true;
+    power-profiles-daemon.enable = false;
+
+    libinput = {
+      enable = true;
+      touchpad = {
+        naturalScrolling = true;
+        tapping = true;
+        disableWhileTyping = false;
+        accelSpeed = "0.5";
+        clickMethod = "clickfinger";
+        tappingDragLock = false;
+        scrollMethod = "twofinger";
+      };
+    };
+
+    fprintd.enable = true;
+    udisks2.enable = true;
+    gvfs.enable = true;
+    fstrim.enable = true;
+
+    logind = {
+      lidSwitch = "suspend";
+      lidSwitchDocked = "ignore";
+      lidSwitchExternalPower = "suspend";
+
+      extraConfig = ''
+        HandlePowerKey=suspend
+        LockSessions=yes
+				IdleAction=suspend
+        IdleActionSec=30min
+      '';
+    };
+  };
+
+	fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-color-emoji
@@ -22,92 +89,4 @@
     font-awesome
     jetbrains-mono
   ];
-
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      START_CHARGE_THRESH_BAT0 = 20;
-      STOP_CHARGE_THRESH_BAT0 = 80;
-
-      USB_AUTOSUSPEND = true;
-
-      DISK_DEVICES = "nvme0n1 sda";
-      DISK_APM_LEVEL_ON_AC = "254 254";
-      DISK_APM_LEVEL_ON_BAT = "128 128";
-
-      WIFI_PWR_ON_AC = "off";
-      WIFI_PWR_ON_BAT = "on";
-    };
-  };
-
-  services.thermald.enable = true;
-
-  services.libinput = {
-    enable = true;
-    touchpad = {
-      naturalScrolling = true;
-      tapping = true;
-      disableWhileTyping = false;
-      accelSpeed = "0.5";
-      clickMethod = "clickfinger";
-      tappingDragLock = false;
-      scrollMethod = "twofinger";
-    };
-  };
-
-  programs.light.enable = true;
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-        Experimental = true;
-      };
-    };
-  };
-  services.blueman.enable = true;
-
-  services.fprintd.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  services.udisks2.enable = true;
-  services.gvfs.enable = true;
-
-  services.logind = {
-    settings = {
-      Login.HandleLidSwitch = "suspend";
-      Login.HandleLidSwitchDocked = "ignore";
-      Login.HandleLidSwitchExternalPower = "suspend";
-    };
-
-    settings.Login = {
-      HandlePowerKey = "suspend";
-      IdleAction = "suspend";
-      IdleActionSec = "30min";
-    };
-  };
-
-  # TLP conflicts with power-profiles-daemon; explicitly disable
-  # the latter since we manage power with TLP on laptops.
-  services.power-profiles-daemon.enable = false;
-
-  hardware.enableRedistributableFirmware = true;
-
-  services.fwupd.enable = true;
-
-  services.fstrim.enable = true;
-
-  boot.kernelParams = [ "nvme_core.default_ps_max_latency_us=0" ];
 }
