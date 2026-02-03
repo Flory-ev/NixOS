dc7ad37d58553e4ca1ed969a031e5ba7fce32230
F <vladislavtkachuk@yahoo.com>
2026-02-03 21:04:23 +0100

    -

--- home.nix
+++ home.nix
@@ -2,159 +2,93 @@
   config,
   pkgs,
   lib,
-  variables,
   ...
 }:
-
 {
   home = {
-    username = variables.username;
-    homeDirectory = "/home/${variables.username}";
-    stateVersion = variables.stateVersion;
+    homeDirectory = "/home/f";
+    stateVersion = "25.05";
+    username = "f";
 
-    sessionVariables = {
-      EDITOR = variables.editor;
-      BROWSER = variables.browser;
-      TERMINAL = variables.terminal;
-      XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
-      XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
-      XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
-    };
-
-    sessionPath = [
-      "${config.home.homeDirectory}/.local/bin"
+    packages = with pkgs; [
+      antigravity
+      bitwarden-desktop
+      chromium
+      discord
+      firefox
+      kitty
+      lutris
+      ncdu
+      qbittorrent
+      reaper
+      spotify
+      telegram-desktop
+      thunderbird
+      tor-browser
+      tree
+      vlc
+      vscodium
     ];
   };
 
-  programs.home-manager.enable = true;
-
-  # Packages
-  home.packages = with pkgs; [
-    # Browser
-    (if variables.browser == "chromium" then chromium else firefox)
-
-    # Editor
-    (if variables.editor == "vscodium" then vscodium else neovim)
-
-    # Terminal
-    (if variables.terminal == "kitty" then kitty else alacritty)
-
-    # Terminal multiplexer
-    (if variables.terminalMultiplexer == "tmux" then tmux else null)
-
-    # Languages
-    nodejs_20
-    python3
-    rustc
-    cargo
-    go
-
-    # Dev tools
-    gh
-    lazygit
-
-    # Communication
-    (lib.mkIf variables.apps.discord discord)
-    (lib.mkIf variables.apps.telegram telegram-desktop)
-
-    # Media
-    (lib.mkIf variables.apps.vlc vlc)
-    (lib.mkIf variables.apps.spotify spotify)
-
-    # Office
-    (lib.mkIf variables.apps.libreoffice libreoffice-qt6-fresh)
-    obsidian
-
-    # Utils
-    pavucontrol
-    gnome-calculator
-  ];
-
-  # Git
-  programs.git = {
-    enable = true;
-    userName = variables.fullName;
-    userEmail = variables.email;
-    extraConfig = {
-      init.defaultBranch = variables.git.defaultBranch;
-      pull.rebase = variables.git.pullRebase;
-      push.autoSetupRemote = true;
-    };
-  };
-
-  # Zsh
-  programs.zsh = lib.mkIf (variables.defaultShell == "zsh") {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    
-    oh-my-zsh = {
+  programs = {
+    bat = {
       enable = true;
-      plugins = [ "git" "sudo" ];
+      config.theme = "TwoDark";
     };
 
-    shellAliases = {
-      ll = "ls -l";
-      la = "ls -la";
-      ".." = "cd ..";
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
     };
-  };
 
-  # Kitty
-  programs.kitty = lib.mkIf (variables.terminal == "kitty") {
-    enable = true;
-    theme = if variables.theme.style == "dark" then "Tokyo Night" else "GitHub Light";
-    font = {
-      name = variables.theme.font.mono;
-      size = variables.theme.font.size;
+    eza = {
+      enable = true;
+      enableZshIntegration = true;
+      git = true;
+      icons = true;
     };
-    settings = {
-      background_opacity = "0.95";
-      confirm_os_window_close = 0;
+
+    firefox.enable = true;
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
     };
-  };
 
-  # Tmux
-  programs.tmux = lib.mkIf (variables.terminalMultiplexer == "tmux") {
-    enable = true;
-    terminal = "tmux-256color";
-    mouse = true;
-    keyMode = "vi";
-    extraConfig = ''
-      set -g status-style 'bg=#1e1e2e fg=#cdd6f4'
-    '';
-  };
+    git = {
+      enable = true;
+			settings = {
+				name = "F";
+				mail = "vladislavtkachuk@yahoo.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+      };
+    };
 
-  # Direnv
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+    home-manager.enable = true;
 
-  # Eza (ls replacement)
-  programs.eza = {
-    enable = true;
-    enableZshIntegration = true;
-    git = true;
-    icons = true;
-  };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
 
-  # Bat (cat replacement)
-  programs.bat = {
-    enable = true;
-    config.theme = if variables.theme.style == "dark" then "TwoDark" else "GitHub";
-  };
-
-  # Fzf
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  # Zoxide
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
+    zsh = {
+      autosuggestion.enable = true;
+      enable = true;
+      enableCompletion = true;
+			syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "sudo" ];
+      };
+      shellAliases = {
+        ".." = "cd ..";
+        la = "ls -la";
+        ll = "ls -l";
+      };
+    };
   };
 }
