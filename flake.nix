f18cc04381c7f8808bd765d2ccc6250d570520b0
F <vladislavtkachuk@yahoo.com>
2026-02-27 20:04:00 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   description = "Vortex";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-25.11";
+      unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
