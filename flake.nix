c7fb0c0c93c271e36ad6ad77b91edf13567ea4e8
F <vladislavtkachuk@yahoo.com>
2026-03-22 18:25:06 +0100

    -

--- flake.nix
+++ flake.nix
@@ -2,20 +2,15 @@
   description = "Vortex";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nh = {
       url = "github:nix-community/nh";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     stylix.url = "github:danth/stylix";
   };
 
@@ -29,11 +24,9 @@
       nixosConfigurations.vortex = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
-
         modules = [
           ./hardware-configuration.nix
           inputs.home-manager.nixosModules.home-manager
-
           (
             {
               lib,
@@ -42,11 +35,9 @@
               ...
             }:
             {
-              # ------------------------------------------------------------------ #
-              #  Nix / nixpkgs                                                     #
-              # ------------------------------------------------------------------ #
-              environment.etc."nix/inputs/nixpkgs".source = inputs.nixpkgs.outPath;
 
+              # --- Nix ---
+              environment.etc."nix/inputs/nixpkgs".source = inputs.nixpkgs.outPath;
               nix.settings = {
                 auto-optimise-store = true;
                 download-buffer-size = 200000000;
@@ -57,30 +48,22 @@
                 flake-registry = "";
                 nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
                 substituters = [ "https://cache.nixos.org/" ];
-                trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                ];
+                trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
               };
-
               nixpkgs.config.allowUnfree = true;
-
               system.stateVersion = "25.05";
 
-              # ------------------------------------------------------------------ #
-              #  Boot                                                               #
-              # ------------------------------------------------------------------ #
+              # --- Boot ---
               boot = {
                 initrd = {
                   systemd.enable = true;
                   verbose = true;
                 };
-
                 kernel.sysctl = {
                   "kernel.dmesg_restrict" = true;
                   "kernel.kptr_restrict" = 2;
                   "kernel.unprivileged_bpf_disabled" = 1;
                 };
-
                 kernelModules = [
                   "btusb"
                   "kvm-amd"
@@ -99,7 +82,6 @@
                   "amd_iommu=on"
                   "iommu=pt"
                 ];
-
                 loader = {
                   efi.canTouchEfiVariables = true;
                   timeout = 5;
@@ -110,26 +92,20 @@
                     configurationLimit = 10;
                   };
                 };
-
                 plymouth = {
                   enable = true;
                   theme = "breeze";
                 };
-
                 tmp.cleanOnBoot = true;
               };
-
               zramSwap = {
                 enable = true;
                 algorithm = "zstd";
                 memoryPercent = 50;
               };
 
-              # ------------------------------------------------------------------ #
-              #  Locale                                                             #
-              # ------------------------------------------------------------------ #
+              # --- Locale ---
               time.timeZone = "Europe/Copenhagen";
-
               i18n = {
                 defaultLocale = "en_US.UTF-8";
                 supportedLocales = [
@@ -137,24 +113,19 @@
                   "ru_RU.UTF-8/UTF-8"
                 ];
               };
-
               console = {
                 font = "Lat2-Terminus16";
                 keyMap = "dk";
                 packages = [ pkgs.terminus_font ];
               };
 
-              # ------------------------------------------------------------------ #
-              #  Networking                                                         #
-              # ------------------------------------------------------------------ #
+              # --- Networking ---
               networking = {
                 hostName = "vortex";
                 nameservers = [
                   "1.1.1.1"
                   "8.8.8.8"
                 ];
-                wireguard.enable = false;
-
                 firewall = {
                   enable = true;
                   allowPing = true;
@@ -162,7 +133,6 @@
                   allowedUDPPorts = [ 7777 ];
                   logRefusedConnections = false;
                 };
-
                 networkmanager = {
                   enable = true;
                   dns = "systemd-resolved";
@@ -172,12 +142,9 @@
                   };
                 };
               };
-
               services.resolved.enable = true;
 
-              # ------------------------------------------------------------------ #
-              #  Hardware                                                           #
-              # ------------------------------------------------------------------ #
+              # --- Hardware ---
               hardware = {
                 bluetooth = {
                   enable = true;
@@ -193,12 +160,9 @@
                 ksm.enable = true;
               };
 
-              # ------------------------------------------------------------------ #
-              #  Desktop / Audio / Fonts                                           #
-              # ------------------------------------------------------------------ #
+              # --- Desktop / Audio / Fonts ---
               services.displayManager.cosmic-greeter.enable = true;
               services.desktopManager.cosmic.enable = true;
-
               services.pipewire = {
                 enable = true;
                 pulse.enable = true;
@@ -209,7 +173,6 @@
                   support32Bit = true;
                 };
               };
-
               fonts = {
                 fontconfig.enable = true;
                 packages = with pkgs; [
@@ -222,30 +185,24 @@
                 ];
               };
 
-              # ------------------------------------------------------------------ #
-              #  Programs                                                           #
-              # ------------------------------------------------------------------ #
+              # --- Programs ---
               programs = {
                 firefox.enable = true;
                 gamemode.enable = true;
-
                 steam = {
                   enable = true;
                   remotePlay.openFirewall = true;
                 };
-
                 zsh = {
                   enable = true;
                   enableCompletion = true;
                   autosuggestions.enable = true;
                   syntaxHighlighting.enable = true;
                 };
-
                 appimage = {
                   enable = true;
                   binfmt = true;
                 };
-
                 nix-ld = {
                   enable = true;
                   libraries = with pkgs; [
@@ -253,7 +210,6 @@
                     zlib
                   ];
                 };
-
                 nh = {
                   enable = true;
                   flake = "/home/f/vortex";
@@ -262,16 +218,12 @@
                     extraArgs = "--keep 3 --keep-since 4d";
                   };
                 };
-
                 virt-manager.enable = true;
               };
 
-              # ------------------------------------------------------------------ #
-              #  Services                                                           #
-              # ------------------------------------------------------------------ #
+              # --- Services ---
               services = {
                 flatpak.enable = true;
-
                 tlp = {
                   enable = true;
                   settings = {
@@ -281,7 +233,6 @@
                     STOP_CHARGE_THRESH_BAT0 = 80;
                   };
                 };
-
                 libinput = {
                   enable = true;
                   touchpad = {
@@ -290,61 +241,51 @@
                     disableWhileTyping = false;
                   };
                 };
-
                 earlyoom = {
                   enable = true;
                   freeMemThreshold = 5;
                   freeSwapThreshold = 10;
                 };
-
                 fstrim.enable = true;
                 fwupd.enable = true;
                 logrotate.enable = true;
                 smartd.enable = true;
                 thermald.enable = true;
                 power-profiles-daemon.enable = lib.mkForce false;
-
                 locate = {
                   enable = true;
                   package = pkgs.plocate;
                   interval = "daily";
                 };
-
-                restic.backups = {
-                  daily = {
-                    repository = "/run/media/f/Backup/vortex-backup";
-                    passwordFile = "/etc/restic/password";
-                    paths = [
-                      "/home/f"
-                      "/etc"
-                    ];
-                    exclude = [
-                      ".cache"
-                      "Downloads"
-                      "node_modules"
-                      "target"
-                    ];
-                    timerConfig = {
-                      OnCalendar = "02:00";
-                      RandomizedDelaySec = "1h";
-                    };
-                    pruneOpts = [
-                      "--keep-daily 7"
-                      "--keep-weekly 4"
-                      "--keep-monthly 6"
-                    ];
+                restic.backups.daily = {
+                  repository = "/run/media/f/Backup/vortex-backup";
+                  passwordFile = "/etc/restic/password";
+                  paths = [
+                    "/home/f"
+                    "/etc"
+                  ];
+                  exclude = [
+                    ".cache"
+                    "Downloads"
+                    "node_modules"
+                    "target"
+                  ];
+                  timerConfig = {
+                    OnCalendar = "02:00";
+                    RandomizedDelaySec = "1h";
                   };
+                  pruneOpts = [
+                    "--keep-daily 7"
+                    "--keep-weekly 4"
+                    "--keep-monthly 6"
+                  ];
                 };
               };
 
-              # ------------------------------------------------------------------ #
-              #  Security                                                           #
-              # ------------------------------------------------------------------ #
+              # --- Security ---
               security.sudo.wheelNeedsPassword = true;
 
-              # ------------------------------------------------------------------ #
-              #  Virtualisation                                                     #
-              # ------------------------------------------------------------------ #
+              # --- Virtualisation ---
               virtualisation = {
                 docker = {
                   enable = true;
@@ -359,12 +300,10 @@
                     dates = "weekly";
                   };
                 };
-
                 podman = {
                   enable = true;
                   defaultNetwork.settings.dns_enabled = true;
                 };
-
                 libvirtd = {
                   enable = true;
                   qemu = {
@@ -374,13 +313,10 @@
                   onBoot = "start";
                   onShutdown = "shutdown";
                 };
-
                 spiceUSBRedirection.enable = true;
               };
 
-              # ------------------------------------------------------------------ #
-              #  Users                                                              #
-              # ------------------------------------------------------------------ #
+              # --- Users ---
               users.users.f = {
                 description = "F";
                 isNormalUser = true;
@@ -398,31 +334,26 @@
                 ];
               };
 
-              # ------------------------------------------------------------------ #
-              #  System packages                                                    #
-              # ------------------------------------------------------------------ #
-              environment.systemPackages = with pkgs; [
-                curl
-                nixfmt
-                wget
-                restic
-                virt-viewer
-                libguestfs
-                guestfs-tools
-                spice
-                spice-gtk
-                spice-protocol
-                virtio-win
-                win-spice
-              ];
-
-              environment.sessionVariables = {
-                LIBVIRT_DEFAULT_URI = "qemu:///system";
+              # --- Environment ---
+              environment = {
+                systemPackages = with pkgs; [
+                  curl
+                  nixfmt
+                  restic
+                  wget
+                  guestfs-tools
+                  libguestfs
+                  spice
+                  spice-gtk
+                  spice-protocol
+                  virt-viewer
+                  virtio-win
+                  win-spice
+                ];
+                sessionVariables.LIBVIRT_DEFAULT_URI = "qemu:///system";
               };
 
-              # ------------------------------------------------------------------ #
-              #  Home Manager                                                       #
-              # ------------------------------------------------------------------ #
+              # --- Home Manager ---
               home-manager = {
                 extraSpecialArgs = { inherit inputs; };
                 useGlobalPkgs = false;
@@ -431,12 +362,10 @@
                   { pkgs, inputs, ... }:
                   {
                     nixpkgs.config.allowUnfree = true;
-
                     home = {
                       username = "f";
                       homeDirectory = "/home/f";
                       stateVersion = "25.05";
-
                       packages = with pkgs; [
                         antigravity
                         bat
@@ -447,7 +376,6 @@
                         fd
                         fzf
                         lutris
-                        nixfmt
                         qbittorrent
                         reaper
                         spotify
@@ -461,7 +389,6 @@
                         zoxide
                       ];
                     };
-
                     programs = {
                       git = {
                         enable = true;
@@ -473,13 +400,11 @@
                           push.autoSetupRemote = true;
                         };
                       };
-
                       zsh = {
                         enable = true;
                         enableCompletion = true;
                         autosuggestion.enable = true;
                         syntaxHighlighting.enable = true;
-
                         oh-my-zsh = {
                           enable = true;
                           plugins = [
@@ -487,7 +412,6 @@
                             "sudo"
                           ];
                         };
-
                         shellAliases = {
                           boot = "nh os boot";
                           clean = "nh clean all";
@@ -495,27 +419,10 @@
                         };
                       };
                     };
-
                     services = { };
-
-                    # Uncomment to enable Stylix theming:
-                    # imports = [ inputs.stylix.homeModules.stylix ];
-                    # stylix = {
-                    #   enable = true;
-                    #   image = pkgs.nixos-artwork.wallpapers.nineish-dark-gray.gnomeFilePath;
-                    #   base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-                    #   cursor = { package = pkgs.bibata-cursors; name = "Bibata-Modern-Ice"; size = 20; };
-                    #   fonts = {
-                    #     monospace = { package = pkgs.jetbrains-mono; name = "JetBrains Mono"; };
-                    #     sansSerif = { package = pkgs.noto-fonts; name = "Noto Sans"; };
-                    #     serif = { package = pkgs.noto-fonts; name = "Noto Serif"; };
-                    #     sizes = { applications = 12; terminal = 14; desktop = 11; popups = 10; };
-                    #   };
-                    #   opacity = { applications = 0.9; terminal = 0.8; desktop = 0.95; popups = 0.85; };
-                    #   targets = { gnome.enable = true; gtk.enable = true; };
-                    # };
                   };
               };
+
             }
           )
         ];
