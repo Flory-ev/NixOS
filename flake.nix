269b8a1b5cf4f4ebea81ac04c6483d1b6cd17f0c
F <vladislavtkachuk@yahoo.com>
2026-04-13 22:15:22 +0200

    -

--- flake.nix
+++ flake.nix
@@ -20,268 +20,6 @@
             { pkgs, ... }:
             {
 
-              # --- Home Manager ---
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.f =
-                  { pkgs, ... }:
-                  {
-                    home = {
-                      username = "f";
-                      homeDirectory = "/home/f";
-                      stateVersion = "25.05";
-                      packages = with pkgs; [
-                        antigravity
-                        bat
-                        eza
-                        fd
-                        fzf
-                        qbittorrent
-                        reaper
-                        spotify
-                        telegram-desktop
-                        tor-browser
-                        tree
-                        zoxide
-                      ];
-                    };
-                    programs = {
-                      git = {
-                        enable = true;
-                        settings = {
-                          user.name = "F";
-                          user.email = "vladislavtkachuk@yahoo.com";
-                          init.defaultBranch = "main";
-                          pull.rebase = true;
-                          push.autoSetupRemote = true;
-                        };
-                      };
-                      zsh = {
-                        enable = true;
-                        autosuggestion.enable = true;
-                        syntaxHighlighting.enable = true;
-                        oh-my-zsh = {
-                          enable = true;
-                          plugins = [
-                            "git"
-                            "sudo"
-                          ];
-                        };
-                        shellAliases = {
-                          boot = "nh os boot";
-                          clean = "nh clean all";
-                          switch = "nh os switch";
-                        };
-                      };
-                    };
-                  };
-              };
-
-              # --- Environment ---
-              environment = {
-                systemPackages = with pkgs; [
-                  curl
-                  nixfmt
-                  wget
-                  spice
-                  spice-gtk
-                  virt-viewer
-                  virtio-win
-                  win-spice
-                ];
-                sessionVariables.LIBVIRT_DEFAULT_URI = "qemu:///system";
-              };
-
-              # --- Virtualisation ---
-              virtualisation = {
-                docker = {
-                  enable = true;
-                  autoPrune = {
-                    enable = true;
-                    flags = [
-                      "--all"
-                      "--filter"
-                      "until=24h"
-                    ];
-                    dates = "weekly";
-                  };
-                };
-                podman = {
-                  enable = true;
-                  defaultNetwork.settings.dns_enabled = true;
-                };
-                libvirtd = {
-                  enable = true;
-                  qemu = {
-                    package = pkgs.qemu_kvm;
-                    runAsRoot = false;
-                    swtpm.enable = true;
-                  };
-                  onBoot = "start";
-                  onShutdown = "shutdown";
-                };
-                spiceUSBRedirection.enable = true;
-              };
-
-              # --- Services ---
-              services = {
-                flatpak.enable = true;
-                libinput.touchpad = {
-                  tapping = true;
-                  naturalScrolling = true;
-                  disableWhileTyping = false;
-                };
-                earlyoom = {
-                  enable = true;
-                  freeMemThreshold = 5;
-                  freeSwapThreshold = 10;
-                };
-                fstrim.enable = true;
-                fwupd.enable = true;
-                restic.backups.daily = {
-                  repository = "/run/media/f/Backup/vortex-backup";
-                  passwordFile = "/etc/restic/password";
-                  paths = [
-                    "/home/f"
-                    "/etc"
-                  ];
-                  exclude = [
-                    ".cache"
-                    "Downloads"
-                    "node_modules"
-                    "target"
-                  ];
-                  timerConfig = {
-                    OnCalendar = "02:00";
-                    RandomizedDelaySec = "1h";
-                  };
-                  pruneOpts = [
-                    "--keep-daily 7"
-                    "--keep-weekly 4"
-                    "--keep-monthly 6"
-                  ];
-                };
-                resolved.enable = true;
-                displayManager.cosmic-greeter.enable = true;
-                desktopManager.cosmic.enable = true;
-                pipewire = {
-                  enable = true;
-                  pulse.enable = true;
-                  jack.enable = true;
-                  alsa = {
-                    enable = true;
-                    support32Bit = true;
-                  };
-                };
-              };
-
-              # --- Programs ---
-              programs = {
-                firefox.enable = true;
-                gamemode.enable = true;
-                steam = {
-                  enable = true;
-                  remotePlay.openFirewall = true;
-                  extraCompatPackages = with pkgs; [ proton-ge-bin ];
-                };
-                zsh.enable = true;
-                appimage = {
-                  enable = true;
-                  binfmt = true;
-                };
-                nix-ld = {
-                  enable = true;
-                  libraries = with pkgs; [
-                    stdenv.cc.cc
-                    zlib
-                  ];
-                };
-                nh = {
-                  enable = true;
-                  flake = "/home/f/vortex";
-                  clean = {
-                    enable = true;
-                    extraArgs = "--keep 3 --keep-since 4d";
-                  };
-                };
-                virt-manager.enable = true;
-              };
-
-              # --- Fonts ---
-              fonts.packages = with pkgs; [
-                fira-code
-                font-awesome
-                jetbrains-mono
-                noto-fonts
-                noto-fonts-cjk-sans
-                noto-fonts-color-emoji
-              ];
-
-              # --- Users ---
-              users.users.f = {
-                description = "F";
-                isNormalUser = true;
-                shell = pkgs.zsh;
-                extraGroups = [
-                  "docker"
-                  "input"
-                  "kvm"
-                  "libvirtd"
-                  "networkmanager"
-                  "video"
-                  "wheel"
-                ];
-              };
-
-              # --- Networking ---
-              networking = {
-                hostName = "vortex";
-                nameservers = [
-                  "1.1.1.1"
-                  "8.8.8.8"
-                ];
-                firewall = {
-                  enable = true;
-                  allowedTCPPorts = [ 7777 ];
-                  allowedUDPPorts = [ 7777 ];
-                  logRefusedConnections = false;
-                };
-                networkmanager = {
-                  enable = true;
-                  dns = "systemd-resolved";
-                  wifi = {
-                    backend = "iwd";
-                    powersave = false;
-                  };
-                };
-              };
-
-              # --- Hardware ---
-              hardware = {
-                bluetooth = {
-                  enable = true;
-                  powerOnBoot = true;
-                  settings.General.Experimental = true;
-                };
-                enableRedistributableFirmware = true;
-                graphics.enable32Bit = true;
-              };
-
-              # --- Locale ---
-              time.timeZone = "Europe/Copenhagen";
-              i18n = {
-                defaultLocale = "en_US.UTF-8";
-                supportedLocales = [
-                  "en_US.UTF-8/UTF-8"
-                  "ru_RU.UTF-8/UTF-8"
-                ];
-              };
-              console = {
-                font = "Lat2-Terminus16";
-                keyMap = "dk";
-              };
-
               # --- Boot ---
               boot = {
                 initrd.systemd.enable = true;
@@ -300,7 +38,6 @@
                 ];
                 kernelPackages = pkgs.linuxPackages_latest;
                 kernelParams = [
-                  "mitigations=auto"
                   "quiet"
                   "intel_iommu=on"
                   "iommu=pt"
@@ -318,24 +55,210 @@
               };
               zramSwap.enable = true;
 
+              # --- System ---
+              time.timeZone = "Europe/Copenhagen";
+              i18n.supportedLocales = [
+                "en_US.UTF-8/UTF-8"
+                "ru_RU.UTF-8/UTF-8"
+              ];
+              console.keyMap = "us";
+
+              # --- Hardware ---
+              hardware = {
+                bluetooth = {
+                  enable = true;
+                  powerOnBoot = true;
+                  settings.General.Experimental = true;
+                };
+                graphics.enable32Bit = true;
+              };
+
+              # --- Networking ---
+              networking = {
+                hostName = "vortex";
+                firewall = {
+                  allowedTCPPorts = [ 7777 ];
+                  allowedUDPPorts = [ 7777 ];
+                };
+                networkmanager = {
+                  enable = true;
+                  dns = "systemd-resolved";
+                  wifi = {
+                    backend = "iwd";
+                    powersave = false;
+                  };
+                };
+              };
+
+              # --- Services ---
+              services = {
+                displayManager.cosmic-greeter.enable = true;
+                desktopManager.cosmic.enable = true;
+                pipewire = {
+                  enable = true;
+                  pulse.enable = true;
+                  jack.enable = true;
+                  alsa = {
+                    enable = true;
+                    support32Bit = true;
+                  };
+                };
+                earlyoom = {
+                  enable = true;
+                  freeMemThreshold = 5;
+                  freeSwapThreshold = 10;
+                };
+                flatpak.enable = true;
+                fstrim.enable = true;
+                fwupd.enable = true;
+              };
+
+              # --- Virtualisation ---
+              virtualisation = {
+                libvirtd = {
+                  enable = true;
+                  qemu = {
+                    package = pkgs.qemu_kvm;
+                    runAsRoot = false;
+                    swtpm.enable = true;
+                  };
+                  onBoot = "start";
+                  onShutdown = "shutdown";
+                };
+                spiceUSBRedirection.enable = true;
+              };
+              environment = {
+                systemPackages = with pkgs; [
+                  curl
+                  nixfmt
+                  wget
+                  spice
+                  spice-gtk
+                  virt-viewer
+                  virtio-win
+                  win-spice
+                ];
+                sessionVariables.LIBVIRT_DEFAULT_URI = "qemu:///system";
+              };
+
+              # --- Programs ---
+              programs = {
+                appimage = {
+                  enable = true;
+                  binfmt = true;
+                };
+                firefox.enable = true;
+                gamemode.enable = true;
+                nh = {
+                  enable = true;
+                  flake = "/home/f/vortex";
+                  clean = {
+                    enable = true;
+                    extraArgs = "--keep 3 --keep-since 4d";
+                  };
+                };
+                nix-ld = {
+                  enable = true;
+                  libraries = with pkgs; [
+                    stdenv.cc.cc
+                    zlib
+                  ];
+                };
+                steam = {
+                  enable = true;
+                  remotePlay.openFirewall = true;
+                  extraCompatPackages = with pkgs; [ proton-ge-bin ];
+                };
+                virt-manager.enable = true;
+                zsh.enable = true;
+              };
+
+              # --- Fonts ---
+              fonts.packages = with pkgs; [
+                fira-code
+                font-awesome
+                jetbrains-mono
+                noto-fonts
+                noto-fonts-cjk-sans
+                noto-fonts-color-emoji
+              ];
+
+              # --- Users ---
+              users.users.f = {
+                isNormalUser = true;
+                shell = pkgs.zsh;
+                extraGroups = [
+                  "input"
+                  "kvm"
+                  "libvirtd"
+                  "networkmanager"
+                  "video"
+                  "wheel"
+                ];
+              };
+
+              # --- Home Manager ---
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.f = _: {
+                  home = {
+                    username = "f";
+                    homeDirectory = "/home/f";
+                    stateVersion = "25.05";
+                    packages = with pkgs; [
+                      antigravity
+                      bat
+                      eza
+                      fd
+                      fzf
+                      qbittorrent
+                      reaper
+                      spotify
+                      telegram-desktop
+                      tor-browser
+                      tree
+                      zoxide
+                    ];
+                  };
+                  programs = {
+                    git = {
+                      enable = true;
+                      settings = {
+                        user.name = "F";
+                        user.email = "vladislavtkachuk@yahoo.com";
+                        init.defaultBranch = "main";
+                        pull.rebase = true;
+                        push.autoSetupRemote = true;
+                      };
+                    };
+                    zsh = {
+                      enable = true;
+                      autosuggestion.enable = true;
+                      syntaxHighlighting.enable = true;
+                      oh-my-zsh = {
+                        enable = true;
+                        plugins = [
+                          "git"
+                          "sudo"
+                        ];
+                      };
+                    };
+                  };
+                };
+              };
+
               # --- Nix ---
               nix.settings = {
                 experimental-features = [
                   "nix-command"
                   "flakes"
                 ];
-                max-jobs = "auto";
-                cores = 0;
-                substituters = [
-                  "https://cache.nixos.org"
-                  "https://nix-community.cachix.org"
-                ];
-                trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                  "nix-community.cachix.org-1:mB9FSh9qf2dde0enMB6oXQ5yOtIyBTD6jLMOx3SoLDA="
-                ];
-                stateVersion = "25.05";
+                substituters = [ "https://nix-community.cachix.org" ];
+                trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dde0enMB6oXQ5yOtIyBTD6jLMOx3SoLDA=" ];
               };
+              nixpkgs.config.allowUnfree = true;
+              system.stateVersion = "25.05";
 
             }
           )
