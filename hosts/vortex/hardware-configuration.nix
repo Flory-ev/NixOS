a16aa24f28d896d6b684c97aeaad407d28dd2475
F <vladislavtkachuk@yahoo.com>
2026-05-21 10:58:25 +0200

    -

--- hosts/vortex/hardware-configuration.nix
+++ hosts/vortex/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "ahci" "nvme" "sdhci_pci" "xhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
