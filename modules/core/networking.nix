9d6ffc4885613ad8d2d28144a853acfdb5d7e8fb
F <vladislavtkachuk@yahoo.com>
2026-06-10 17:50:25 +0200

    -

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -15,4 +15,17 @@
       };
     };
   };
+
+  # --- iwd settings: prefer 5 GHz to avoid 2.4 GHz congestion ---
+  networking.wireless.iwd = {
+    enable = true;
+    settings = {
+      General = {
+        RoamRetryInterval = 15;
+      };
+      Rank = {
+        BandModifier5Ghz = 2.0; # Strongly prefer 5 GHz networks
+      };
+    };
+  };
 }
