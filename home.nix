12a693695f279864c8e76ed3dbfbfa8883beb9aa
F <vladislavtkachuk@yahoo.com>
2026-07-20 00:43:54 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -6,7 +6,6 @@
     stateVersion = "25.05";
 
     packages = with pkgs; [
-      antigravity
       qbittorrent
       spotify
       termius
@@ -16,6 +15,9 @@
 
   # Programs
   programs = {
+    gh = {
+      enable = true;
+    };
     git = {
       enable = true;
       settings = {
@@ -42,75 +44,75 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
     };
-#     plasma = {
-#       enable = true;
-#       overrideConfig = true;
+    plasma = {
+      enable = true;
+      overrideConfig = true;
 
-#       workspace = {
-#         wallpaperPlainColor = "0,0,0";
-#         lookAndFeel = "org.kde.breezedark.desktop";
-#         colorScheme = "BreezeDark";
-#         iconTheme = "breeze-dark";
-#         cursor.theme = "Breeze_Snow";
-#       };
+      workspace = {
+        wallpaperPlainColor = "0,0,0";
+        lookAndFeel = "org.kde.breezedark.desktop";
+        colorScheme = "BreezeDark";
+        iconTheme = "breeze-dark";
+        cursor.theme = "Breeze_Snow";
+      };
 
-#       fonts = let
-#         mono = { family = "JetBrains Mono"; pointSize = 10; };
-#       in {
-#         general = mono;
-#         fixedWidth = mono;
-#         toolbar = mono;
-#         menu = mono;
-#         windowTitle = mono;
-#         small = { family = "JetBrains Mono"; pointSize = 8; };
-#       };
+      fonts = let
+        mono = { family = "JetBrains Mono"; pointSize = 10; };
+      in {
+        general = mono;
+        fixedWidth = mono;
+        toolbar = mono;
+        menu = mono;
+        windowTitle = mono;
+        small = { family = "JetBrains Mono"; pointSize = 8; };
+      };
 
-#       input.keyboard = {
-#         layouts = [
-#           { layout = "us"; }
-#           { layout = "ru"; }
-#         ];
-#         options = [ "grp:logo_space_toggle" ];
-#       };
+      input.keyboard = {
+        layouts = [
+          { layout = "us"; }
+          { layout = "ru"; }
+        ];
+        options = [ "grp:logo_space_toggle" ];
+      };
 
-#       kwin = {
-#         virtualDesktops.number = 1;
+      kwin = {
+        virtualDesktops.number = 1;
 
-#         effects = {
-#           blur.enable = true;
-#           minimization.animation = "magiclamp";
-#         };
+        effects = {
+          blur.enable = true;
+          minimization.animation = "magiclamp";
+        };
 
-#         titlebarButtons = {
-#           right = [
-#             "minimize"
-#             "maximize"
-#             "close"
-#           ];
-#         };
-#       };
+        titlebarButtons = {
+          right = [
+            "minimize"
+            "maximize"
+            "close"
+          ];
+        };
+      };
 
-#       panels = [
-#         {
-#           location = "top";
-#           height = 25;
-#           widgets = [
-#             "org.kde.plasma.panelspacer"
-#             "org.kde.plasma.systemtray"
-#             "org.kde.plasma.digitalclock"
-#           ];
-#         }
-#         {
-#           location = "bottom";
-#           height = 50;
-#           lengthMode = "fit";
-#           floating = true;
-#           widgets = [
-#             "org.kde.plasma.kickoff"
-#             "org.kde.plasma.icontasks"
-#           ];
-#         }
-#       ];
-#     };
-#   };
-# }
+      panels = [
+        {
+          location = "top";
+          height = 25;
+          widgets = [
+            "org.kde.plasma.panelspacer"
+            "org.kde.plasma.systemtray"
+            "org.kde.plasma.digitalclock"
+          ];
+        }
+        {
+          location = "bottom";
+          height = 50;
+          lengthMode = "fit";
+          floating = true;
+          widgets = [
+            "org.kde.plasma.kickoff"
+            "org.kde.plasma.icontasks"
+          ];
+        }
+      ];
+    };
+  };
+}
