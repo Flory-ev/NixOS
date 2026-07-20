c4f84044bdc94eb16f1112e884e4509f20095284
F <vladislavtkachuk@yahoo.com>
2026-07-20 14:48:53 +0200

    -

--- modules/environment.nix
+++ modules/environment.nix
@@ -1,16 +1,16 @@
 { pkgs, ... }:
 {
-  coreutils.package = pkgs.uutils-coreutils;
+  environment = {
+    systemPackages = with pkgs; [
+      nixfmt
+    ];
 
-  environment.systemPackages = with pkgs; [
-    nixfmt
-  ];
-
-  environment.sessionVariables = {
-    PROTON_ENABLE_NVAPI = "1";
-    PROTON_HIDE_NVIDIA_GPU = "0";
-    STEAM_RUNTIME_PREFER_HOST_LIBRARIES = "0";
-    WINE_FULLSCREEN_FSR = "1";
-    WINE_FULLSCREEN_FSR_STRENGTH = "2";
+    sessionVariables = {
+      PROTON_ENABLE_NVAPI = "1";
+      PROTON_HIDE_NVIDIA_GPU = "0";
+      STEAM_RUNTIME_PREFER_HOST_LIBRARIES = "0";
+      WINE_FULLSCREEN_FSR = "1";
+      WINE_FULLSCREEN_FSR_STRENGTH = "2";
+    };
   };
 }
