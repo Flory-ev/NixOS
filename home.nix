f74a1590131d27fd5903da0579f86ff032795a12
F <vladislavtkachuk@yahoo.com>
2026-08-11 15:16:30 +0200

    -

--- home.nix
+++ home.nix
@@ -1,8 +1,11 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   home = {
     username = "f";
     homeDirectory = "/home/f";
+    sessionVariables = {
+      SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/bitwarden-ssh-agent.sock";
+    };
     stateVersion = "26.05";
     packages = with pkgs; [
       bitwarden-desktop
@@ -12,6 +15,7 @@
       spotify
       telegram-desktop
       termius
+      vital
       wineWow64Packages.staging
       winetricks
       yabridge
@@ -33,6 +37,13 @@
       };
     };
     ripgrep.enable = true;
+    ssh = {
+      enable = true;
+      extraConfig = ''
+        Host *
+          IdentityAgent ~/.bitwarden-ssh-agent.sock
+      '';
+    };
     starship = {
       enable = true;
       settings = {
